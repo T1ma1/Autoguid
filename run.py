64f19b2b2cc0ea218faaa268c2a66c8ba3779be0
T1ma1 <timatimpabg@gmail.com>
2025-04-18 00:13:42 +0300

    Fix: corrected _main_ and port binding

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import app, db
+import os
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="localhost", port=8000, debug=True)
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
